7a8894c5250c437f2f83bacbc96f31371f6deafb
slkleyton <kleyton.sartori.leite@gmail.com>
2020-03-27 16:55:12 -0400

    create communication with HA7NET

---
+++ CmtHA7NET.py
@@ -0,0 +1,31 @@
+import requests
+from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup as BS
+
+
+class CmtHA7NET(object):
+    def __init__(self, address):
+        self.ip = address
+
+    def getAllDev(self):
+        """
+            Retorna uma lista de dispositivos no HA7NET
+        """
+        sensors = []
+        try:
+            req = requests.get("http://"+self.ip+"/1Wire/Search.html")
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # Python 3.6
+            return err
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+            return err
+        req.encoding = 'utf-8'
+        soup = BS(req.text, 'html.parser')
+        address = soup.find(id="Addresses").find_all(value=True)
+        id = 0
+        for i in address:
+            if i.attrs['name'] == "Address_"+str(id):
+                sensors.append(i.attrs['value'])
+                id+=1
+        return sensors
