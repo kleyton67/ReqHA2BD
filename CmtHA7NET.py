03dd831049fb667dae6dbcdaae32055e6be3a54a
kleiton67 <kleyton.sartori.leite@gmail.com>
2020-03-29 16:52:00 -0400

    Funçao para requisitar temperaturas do sensor pelo gateway

--- CmtHA7NET.py
+++ CmtHA7NET.py
@@ -27,5 +27,23 @@ class CmtHA7NET(object):
         for i in address:
             if i.attrs['name'] == "Address_"+str(id):
                 sensors.append(i.attrs['value'])
-                id+=1
+                id += 1
         return sensors
+
+    def getSensorsTemp(self,  address, precision):
+        """
+            address: endereco de 16caracteres dos sensores
+            precision: precisão esperada para a leitura das
+                temperaturas
+            retorno: temperatura do sensor desejado
+        """
+        try:
+            req = requests.get("http://"+self.ip+"/1Wire//ReadDS18B20.html?" +
+                "DS18B20Request={"+str((address)+","+str(precision)+"}")
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # Python 3.6
+            return err
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+            return err
+        
